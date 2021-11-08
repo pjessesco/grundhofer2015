9cd2dab4766617ce12c342dd289cdac4be060110
Jino Park <pjessesco@gmail.com>
2021-11-08 23:22:37 +0900

    fix

--- 2_2_apply_w.py
+++ 2_2_apply_w.py
@@ -38,33 +38,22 @@ N = 500
 
 REF_PATH = "./texture/"
 REF_CAPTURED_WARPED_PATH = "./tps_input/"
-INPUT_IMG_PATH = "./ref.png"
+
 color_channel = 3
 STEP = 4
 
-print("Reading images...")
 captured = read_pngs(REF_CAPTURED_WARPED_PATH)
-input_img = read_png(INPUT_IMG_PATH)
-print("Done")
-
-output_img = np.zeros(input_img.shape).astype('float32')
-
-O = np.zeros((4, 4, STEP, img_width))
 
 W = cp.load("W.npy")
-print(W.shape)
 
-print("Preprocess common terms")
-euclid_dist_input_captured_mat = np.zeros((N, img_width, img_height))
-for i in range(N):
-    euclid_dist_input_captured_mat[i] = np.linalg.norm(input_img - captured[i], axis=2)
+def process(input_img):
 
-print("Done")
+    euclid_dist_input_captured_mat = np.zeros((N, img_width, img_height))
+    for i in range(N):
+        euclid_dist_input_captured_mat[i] = np.linalg.norm(input_img - captured[i], axis=2)
 
-euclid_dist_input_captured_mat = cp.asarray(euclid_dist_input_captured_mat)
-input_img = cp.asarray(input_img)
-
-def process():
+    input_img = cp.asarray(input_img)
+    euclid_dist_input_captured_mat = cp.asarray(euclid_dist_input_captured_mat)
 
     ret_val = cp.sum(cp.reshape(cp.concatenate([tps_rbf(euclid_dist_input_captured_mat), cp.zeros((4, img_width, img_height))], axis=0), (N+4, 1, img_width, img_height)) * W, axis=0).get()
 
@@ -80,9 +69,12 @@ def process():
 
 if __name__ == '__main__':
 
+    INPUT_IMG_PATH = "./ref.png"
+    input_img = read_png(INPUT_IMG_PATH)
+
     params = range(0, img_width, STEP)
 
-    output_img = process().astype('float32')
+    output_img = process(input_img).astype('float32')
         
     print(output_img.shape)
     cv2.imwrite("output_texture.png", cv2.cvtColor(output_img, cv2.COLOR_BGR2RGB)*255)
